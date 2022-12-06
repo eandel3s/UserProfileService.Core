b09ff9597e70c13a654fd1afe54093aabb7ea36c
Emmanuel Andela <emmanuel.andela@bechtle.com>
2022-12-06 11:46:05 +0100

    Change summaries in Weatherforecast controller

--- UserProfileServiceCore/src/UserProfileServiceCore/Controllers/WeatherForecastController.cs
+++ UserProfileServiceCore/src/UserProfileServiceCore/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace UserProfileServiceCore.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot like me", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
